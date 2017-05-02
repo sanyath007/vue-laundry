323abdd4d07dc260314362109a26434d4bfe1f18
Kobe <sanyath007@gmail.com>
2017-05-02 10:05:53 +0700

    Setup page structure, main menu and dependencies.
    Setup page structure, main menu and dependencies.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+/** Import event calendar and css file */
+// import 'vue-event-calendar/dist/style.css' // ^1.1.10, CSS has been extracted as one file, so you can easily update it.
+// import vueEventCalendar from 'vue-event-calendar'
+
 Vue.config.productionTip = false
+
+/** Use event calendar and set default language */
+// Vue.use(vueEventCalendar, {locale: 'en'}) // locale can be 'zh' or 'en'
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Home from '@/components/Home'
+import About from '@/components/About'
+import Contact from '@/components/Contact'
+import Signup from '@/components/Signup'
+import Signin from '@/components/Signin'
+// import ReserveList from '@/components/Reservations/reserve-list'
+// import ReserveDetail from '@/components/Reservations/reserve-detail'
+// import ReserveForm from '@/components/Reservations/reserve-form'
+// import ReserveCancel from '@/components/Reservations/reserve-cancel'
 
 Vue.use(Router)
 
@@ -8,8 +16,53 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home',
+      component: Home
+    },
+    // {
+    //   path: '/reservations',
+    //   name: 'ReserveList',
+    //   component: ReserveList
+    // },
+    // {
+    //   path: '/reservation/:id',
+    //   name: 'ReserveDetail',
+    //   component: ReserveDetail
+    // },
+    // {
+    //   path: '/reservation/:id/edit',
+    //   name: 'ReserveForm',
+    //   component: ReserveForm
+    // },
+    // {
+    //   path: '/reserve-cancel',
+    //   name: 'ReserveCancel',
+    //   component: ReserveCancel
+    // },
+    // {
+    //   path: '/reservation',
+    //   name: 'ReserveNew',
+    //   component: ReserveForm
+    // },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin
     }
   ]
 })
